66179b40f7f9974b1d1fa653141bf54108f823f0
Pavan S Patil <psp3075@gmail.com>
2021-10-05 08:57:23 +0530

    pages and comp

---
+++ components/Footer.js
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <div>Made with Next.js</div>;
+};
+
+export default Footer;
---
+++ pages/about.js
@@ -0,0 +1,21 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        perferendis dolorem ipsam, sequi enim minus? Dicta debitis rem
+        asperiores iusto possimus nostrum facere rerum necessitatibus.
+        Asperiores quis placeat nobis consequatur.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate
+        perferendis dolorem ipsam, sequi enim minus? Dicta debitis rem
+        asperiores iusto possimus nostrum facere rerum necessitatibus.
+        Asperiores quis placeat nobis consequatur.
+      </p>
+    </div>
+  );
+};
+
+export default About;
